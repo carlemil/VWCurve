c9f0f5f456fbb8e45a143c0bbd952173e7907fcc
Carl-Emil Kjellstrand <erbnsman@gmail.com>
2020-12-06 20:14:41 +0100

    Added min and max width as params to the line.

--- src/main/kotlin/VariableWidthLine.kt
+++ src/main/kotlin/VariableWidthLine.kt
@@ -2,17 +2,20 @@ package se.kjellstrand.variablewidthline
 
 import java.awt.Color
 import java.awt.Graphics2D
-import java.awt.geom.AffineTransform
 import java.awt.geom.GeneralPath
 import kotlin.math.*
 
 class VariableWidthLine {
 
     companion object {
-        fun drawLine(line: List<LinePoint>,
-                     g2: Graphics2D,
-                     size: Double) {
-            drawPolygon(buildHullFromPolygon(line, size), g2)
+        fun drawLine(
+            line: List<LinePoint>,
+            g2: Graphics2D,
+            size: Double,
+            minWidth: Double,
+            maxWidth: Double
+        ) {
+            drawPolygon(buildHullFromPolygon(line, size, minWidth, maxWidth), g2)
         }
 
         private fun calculateSidesOfTriangle(p0: LinePoint, p1: LinePoint): Triple<Double, Double, Double> {
@@ -22,7 +25,12 @@ class VariableWidthLine {
             return Triple(a, b, c)
         }
 
-        private fun buildHullFromPolygon(ppList: List<LinePoint>, size: Double): MutableList<LinePoint> {
+        private fun buildHullFromPolygon(
+            ppList: List<LinePoint>,
+            size: Double,
+            minWidth: Double,
+            maxWidth: Double
+        ): MutableList<LinePoint> {
             val leftHull = mutableListOf<LinePoint>()
             val rightHull = mutableListOf<LinePoint>()
 
@@ -35,8 +43,8 @@ class VariableWidthLine {
                 val alfaPlus90 = calculateAlfa(a, c, b, (PI / 2.0))
                 val alfaMinus90 = calculateAlfa(a, c, b, -(PI / 2.0))
 
-                leftHull.add(calculatePerpendicularPolyPoint(p0, p1, size, alfaPlus90))
-                rightHull.add(calculatePerpendicularPolyPoint(p0, p1, size, alfaMinus90))
+                leftHull.add(calculatePerpendicularPolyPoint(p0, p1, size, minWidth, maxWidth, alfaPlus90))
+                rightHull.add(calculatePerpendicularPolyPoint(p0, p1, size, minWidth, maxWidth, alfaMinus90))
             }
 
             val hull = mutableListOf<LinePoint>()
@@ -63,12 +71,19 @@ class VariableWidthLine {
             g2.fill(path)
         }
 
-        private fun calculatePerpendicularPolyPoint(p0: LinePoint, p1: LinePoint, size: Double, alfaPlus90: Double): LinePoint {
+        private fun calculatePerpendicularPolyPoint(
+            p0: LinePoint,
+            p1: LinePoint,
+            size: Double,
+            minWidth: Double,
+            maxWidth: Double,
+            alfaPlus90: Double
+        ): LinePoint {
             val (_, _, c) = calculateSidesOfTriangle(p0, p1)
             // * 0.75 (0.75 + 0.10 == 0.85) to set a max width that is close to touching the nearest line.
             // + 0.10 to set a min width that is still visible.
             // * 3 since we use 3 intermediate points when we generate the polygon (the smooth step adds them)
-            val width = (size * c * (((p0.w + p1.w) / 2) * 0.75 + 0.10)) / 2
+            val width = ((p0.w + p1.w) / 2.0) * (maxWidth - minWidth) / 2.0 + minWidth / 2.0
             val x = (p0.x + p1.x) * size / 2.0 + width * sin(alfaPlus90)
             val y = (p0.y + p1.y) * size / 2.0 + width * cos(alfaPlus90)
             return LinePoint(x, y)
